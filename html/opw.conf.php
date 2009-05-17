972bd7dee353f6da9549f58cc2a2c201a5ec4f7b
Brian Tanaka <brian.tanaka@gmail.com>
2009-05-16 17:34:02 -0700

    worked on opww copy

--- html/opw.conf.php
+++ html/opw.conf.php
@@ -4,8 +4,8 @@
 # Opw Template Engine Config
 #
 
-define('SITETITLE', 'One Page Wonder');
-define('TAGLINE', 'This the default tagline defined in OPW config');
+define('SITETITLE', 'One Page Wonder Websites');
+define('TAGLINE', 'Low Price, High Value Websites for Small Businesses');
 define('BASEPATH', '/Users/btanaka/stuffs/stuff.1/o/onepagewonders.d/html');
 define('CSS', './opw.css.default.css');
 // $opwcontent = "user_files/opw.content";
