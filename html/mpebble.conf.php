ff818aa6f0b018d747e571b2731cacafbd939eca
Brian Tanaka <brian.tanaka@gmail.com>
2009-05-21 19:12:16 -0700

    minor

--- html/mpebble.conf.php
+++ html/mpebble.conf.php
@@ -9,7 +9,8 @@ define('TAGLINE', 'A tiny, lightweight, super cool CMS...');
 define('BASEPATH', '/Users/btanaka/stuffs/stuff.1/m/moonpebblecms.d/html');
 define('CSS', './mpebble.css.default.css');
 // $opwcontent = "user_files/opw.content";
-define('MPEBBLECONTENT', './user_files/mpebble.content');
+define('MPEBBLECONTENT', './user_files/mpebble.content.mdwn');
 
 # old tagline define('TAGLINE', 'Low Price, High Value Websites for Small Businesses'); 
 ?>
+
