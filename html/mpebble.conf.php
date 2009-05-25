2a7c566d0857e856bb5bf9e17d0b2e63680262ef
Brian Tanaka <brian.tanaka@gmail.com>
2009-05-24 18:27:14 -0700

    working on some preliminary themes

--- html/mpebble.conf.php
+++ html/mpebble.conf.php
@@ -8,7 +8,9 @@ define('SITETITLE', 'MoonpebbleCMS');
 define('TAGLINE', 'A tiny, lightweight, super cool CMS...');
 define('FOOTER', 'Footers are good eating!');
 define('BASEPATH', '/Users/btanaka/stuffs/stuff.1/m/moonpebblecms.d/html');
-define('MPEBBLETHEME', 'superplain'); // set to theme's name
+// define('MPEBBLETHEME', 'superplain'); // set to theme's name
+// define('MPEBBLETHEME', 'default'); // set to theme's name
+define('MPEBBLETHEME', 'bigheadline'); // set to theme's name
 define('MPEBBLECONTENT', './user_files/mpebble.content.mdwn');
 
 ?>
