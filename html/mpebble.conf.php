727031cb88aebcac750dd85af934d70973e0d3cb
Brian Tanaka <brian.tanaka@gmail.com>
2009-05-29 16:28:10 -0700

    working on greenfade theme

--- html/mpebble.conf.php
+++ html/mpebble.conf.php
@@ -6,14 +6,15 @@
 
 define('SITETITLE', 'MoonpebbleCMS');
 define('TAGLINE', 'A tiny, lightweight, super cool CMS...');
-define('FOOTER', 'Powered by MoonpebbleCMS - Theme: Superplain');
+define('FOOTER', 'Powered by MoonpebbleCMS');
 define('BASEPATH', '/Users/btanaka/stuffs/stuff.1/m/moonpebblecms.d/html');
 define('MPEBBLECONTENT', './user_files/mpebble.content.mdwn');
 
 # choose a theme
-define('MPEBBLETHEME', 'superplain'); // set to theme's name
-//define('MPEBBLETHEME', 'greentop'); // set to theme's name
+//define('MPEBBLETHEME', 'superplain'); // set to theme's name
+define('MPEBBLETHEME', 'greenfade'); // set to theme's name
 //define('MPEBBLETHEME', 'bigheadline'); // set to theme's name
+//define('MPEBBLETHEME', 'greentop'); // set to theme's name
 
 ?>
 
