363c722000c7f77f536d814558f80c3815eb90c0
Brian Tanaka <brian.tanaka@gmail.com>
2009-06-01 17:27:23 -0700

    tweaked themes. added licenses.

--- html/mpebble.conf.php
+++ html/mpebble.conf.php
@@ -11,8 +11,8 @@ define('BASEPATH', '/Users/btanaka/stuffs/stuff.1/m/moonpebblecms.d/html');
 define('MPEBBLECONTENT', './user_files/mpebble.content.mdwn');
 
 # choose a theme
-//define('MPEBBLETHEME', 'superplain'); // set to theme's name
-define('MPEBBLETHEME', 'greenfade'); // set to theme's name
+define('MPEBBLETHEME', 'superplain'); // set to theme's name
+//define('MPEBBLETHEME', 'greenfade'); // set to theme's name
 //define('MPEBBLETHEME', 'bigheadline'); // set to theme's name
 //define('MPEBBLETHEME', 'greentop'); // set to theme's name
 
